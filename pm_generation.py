dc3b5b16fa80c649f543923b737741ab36d08580
in2uitive <heekwon@cs.stonybrook.edu>
2018-07-20 17:13:01 -0400

    First commit

---
+++ pm_generation.py
@@ -0,0 +1,99 @@
+import os
+import sys
+import argparse
+
+from util import load_kb, generate_tmp_file, unlink_tmp_file
+
+def build_arg_parser():
+
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers()
+
+    # Create the parser for the "prepare" command
+    parser_prepare = subparsers.add_parser('prepare')
+    parser_prepare.add_argument('-data_dir', type=str, required=True,
+            help="Path to the dataset")
+    parser_prepare.add_argument('-data_out', type=str, required=True,
+            help="Output file for the prepared data")
+    parser_prepare.set_defaults(func=data_preparation)
+
+    # Create the parser for the "train" command
+    parser_train = subparsers.add_parser('train')
+    parser_train.add_argument('-data', type=str, required=True,
+            help='Path prefix to the ".train.pt" and ".valid.pt" file path')
+    parser_train.add_argument('-model', type=str, required=True,
+            help="Model filename")
+    parser_train.set_defaults(func=train)
+
+    # Create the parser for the "generate" command
+    parser_generate = subparsers.add_parser('generate')
+    parser_generate.add_argument('-data_dir', type=str,
+            required=True, help="Path to the dataset")
+    parser_generate.add_argument('-dataset', type=str,
+            default='valid', help="Dataset for generating post modifier")
+    parser_generate.add_argument('-model', type=str, required=True,
+            help="Path to model .pt file")
+    parser_generate.add_argument('-out', type=str, default="pred.txt",
+            help="Path to output the prediction")
+    parser_generate.set_defaults(func=generate)
+
+    return parser
+
+def data_preparation(args):
+
+    dataset = ['train', 'valid']
+    wiki_kb = {}
+    src = {}
+    tgt = {}
+
+    arguments = ""
+    for set_info in dataset:
+        wiki_kb[set_info] = load_kb(os.path.join(args.data_dir, "{}.wiki".format(set_info)))
+        src[set_info], tgt[set_info] = generate_tmp_file(os.path.join(args.data_dir, set_info), wiki_kb[set_info])
+        arguments += "-{}_src {} -{}_tgt {} ".format(set_info, src[set_info], set_info, tgt[set_info])
+
+    script = "python OpenNMT-py/preprocess.py {}-save_data {} -src_seq_length 1000".format(arguments, args.data_out)
+
+    print(script)
+    os.system(script)
+    for set_info in dataset:
+        unlink_tmp_file(src[set_info])
+        unlink_tmp_file(tgt[set_info])
+
+def train(args):
+
+    rnn_type = "brnn"
+    batch_size = 32
+    attention = "general"
+
+    script = "python OpenNMT-py/train.py -data {} -save_model {} -gpuid 0 \
+            -global_attention {} -batch_size {} -encoder_type {}".format(args.data, args.model, attention, batch_size, rnn_type)
+
+    print(script)
+    os.system(script)
+
+def generate(args):
+
+    wiki_kb = load_kb(os.path.join(args.data_dir, "{}.wiki".format(args.dataset)))
+    src, tgt = generate_tmp_file(os.path.join(args.data_dir, args.dataset), wiki_kb)
+
+    script = "python OpenNMT-py/translate.py -model {} -src {} -output {} -replace_unk -verbose".format(args.model, src, args.out)
+
+    os.system(script)
+
+    unlink_tmp_file(src)
+    unlink_tmp_file(tgt)
+
+def main(argv):
+    if len(argv) < 2:
+        print("Please provide command:\n\t python pm_generation.py [prepare|train|generate]")
+        exit()
+
+    arg_parser = build_arg_parser()
+    in_args = sys.argv[1:]
+    args = arg_parser.parse_args(in_args)
+    args.func(args)
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ util.py
@@ -0,0 +1,57 @@
+import os
+import tempfile
+import json
+
+def load_kb(infile, verbose=False):
+    wiki_kb = {}
+    with open(infile, "r") as fin:
+        for lid, line in enumerate(fin):
+            row = line.strip().split("\t")
+            wiki_kb[row[0]] = {}
+            wiki_kb[row[0]]['label'] = row[1]
+            wiki_kb[row[0]]['aliases'] = row[2].split(",")
+            wiki_kb[row[0]]['description'] = row[3].split(",")
+            wiki_kb[row[0]]['claims_raw'] = row[4]
+            wiki_kb[row[0]]['claims'] = {}
+            for claims in json.loads(row[5]):
+                for k, v in claims.items():
+                    if k == "property":
+                        key = v[0]
+                        if key in wiki_kb[row[0]]['claims']:
+                            wiki_kb[row[0]]['claims'][key].append(v[1])
+                        else:
+                             wiki_kb[row[0]]['claims'][key] = [v[1]]
+            if verbose:
+                for k,v in wiki_kb[row[0]].items():
+                    print("{}:\t{}".format(k, v))
+
+    return wiki_kb
+
+def generate_tmp_file(infile, wiki_kb, verbose=False):
+    _, src_path = tempfile.mkstemp(suffix='.tmp')
+    _, tgt_path = tempfile.mkstemp(suffix='.tmp')
+    #src_path = "{}.src".format(infile)
+    #tgt_path = "{}.tgt".format(infile)
+    with open(infile, "r") as fin, open(src_path, "w") as src_out, open(tgt_path, "w") as tgt_out:
+        for line in fin:
+            row = line.strip().split("\t")
+            source = row[0]
+            claims = []
+            for k, v in wiki_kb[row[4]]['claims'].items():
+                claims.append("<rel> {} </rel> <value> {} </value>".format(k, " <and> ".join(v)))
+
+            source += " " + " ".join(claims)
+            src_out.write(source + "\n")
+            tgt_out.write(row[2] + "\n")
+            if verbose:
+                print("Sent_Wo_PM: {}".format(row[0]))
+                print("Entity: {}".format(row[1]))
+                print("PM: {}".format(row[2]))
+                print("Sent: {}".format(row[3]))
+                print("Wiki_ID: {}".format(row[4]))
+                print("Prev_Sent: {}".format(row[5]))
+
+    return src_path, tgt_path
+
+def unlink_tmp_file(file_path):
+    os.unlink(file_path)
